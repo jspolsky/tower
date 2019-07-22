405c9de155dee23aa5d8074a8f7a5ec2ac468b39
jspolsky <spolsky@Taco.home>
2019-07-22 14:37:10 -0400

    Shoot Up

--- tower.ino
+++ tower.ino
@@ -8,17 +8,98 @@
 
 bool gReverseDirection = false;
 
-CRGB leds[NUM_LEDS][4];
+CRGB leds0[NUM_LEDS];
+CRGB leds1[NUM_LEDS];
+CRGB leds2[NUM_LEDS];
+CRGB leds3[NUM_LEDS];
+
+
+
+int DebugPrintf(char* pszFmt, ... ) {
+
+    int cargs = 0;
+    char* pszTmp;
+
+    for (pszTmp = pszFmt; *pszTmp; pszTmp++)  
+        if (*pszTmp == '%')  
+            cargs++; 
+
+    va_list argv;
+    va_start(argv, cargs);
+
+    pszTmp = pszFmt;
+    while (*pszTmp)
+    {
+        if (*pszTmp == '%')
+        {
+            pszTmp++;
+            switch (*pszTmp)
+            {
+                case 'd':   
+                    Serial.print(va_arg(argv, int));    
+                    break;
+
+                case 'x':   
+                    Serial.print(va_arg(argv, int), HEX);    
+                    break;
+
+                case 'l':   
+                    Serial.print(va_arg(argv, long));
+                    break;
+
+                case 'u':
+                    Serial.print(va_arg(argv, unsigned long));
+                    break;
+
+                case 'f':
+                    Serial.print(va_arg(argv, double));
+                    break;
+
+                case 'F':
+                    Serial.print(va_arg(argv, double), 8);
+                    break;
+
+                case 'c':
+                    Serial.print((char) va_arg(argv, int));
+                    break;
+
+                case 's':
+                    Serial.print(va_arg(argv, char*));
+                    break;
+
+                case '%':
+                    Serial.print('%');
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        else if (*pszTmp == '\n')
+        {
+            Serial.println();
+        }
+        else
+        {
+            Serial.print(*pszTmp);
+        }
+
+        pszTmp++;
+    }
+}
+
+
+
 
 void setup() 
 {
   
   delay(3000); // ?
   
-  FastLED.addLeds<CHIPSET, 5, COLOR_ORDER>(leds[0], NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<CHIPSET, 6, COLOR_ORDER>(leds[1], NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<CHIPSET, 12, COLOR_ORDER>(leds[2], NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<CHIPSET, 9, COLOR_ORDER>(leds[3], NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, 5, COLOR_ORDER>(leds0, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, 6, COLOR_ORDER>(leds1, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, 12, COLOR_ORDER>(leds2, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, 9, COLOR_ORDER>(leds3, NUM_LEDS).setCorrection( TypicalLEDStrip );
   
   FastLED.setBrightness( BRIGHTNESS );
 
@@ -26,21 +107,29 @@ void setup()
 
 void loop()
 {
-  SineWave();
+  ShootUp();
 }
 
 
 
-void SineWave()
+void ShootUp()
 {
-  static uint8_t pos = 0;
+  static long pos = 0;
+  static uint8_t increment = 3;
 
-  fill_solid( &(leds[0][0]), NUM_LEDS*4, CRGB::Black );
+  fill_solid( &(leds0[0]), NUM_LEDS, CRGB::Black );
+  fill_solid( &(leds1[0]), NUM_LEDS, CRGB::Black );
+  fill_solid( &(leds2[0]), NUM_LEDS, CRGB::Black );
+  fill_solid( &(leds3[0]), NUM_LEDS, CRGB::Black );
 
-  for (int i=0; i<3; i++)
-    fill_solid(&(leds[ 0xFF & ((uint16_t) pos * NUM_LEDS / 255) ][i]), 10, CRGB::Red);
+  fill_solid(&(leds0[ pos ]), 10, CRGB::Red);
+  fill_solid(&(leds1[ pos ]), 10, CRGB::Red);
+  fill_solid(&(leds2[ pos ]), 10, CRGB::Red);
+  fill_solid(&(leds3[ pos ]), 10, CRGB::Red);
 
-  pos += 5;
+  pos = (pos + increment) % (NUM_LEDS - 10);
+  increment++;
+  if (pos < 10) increment = 3;
   
   FastLED.show();
   delay(1);
